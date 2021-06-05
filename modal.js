a6963522083fe75159daf6e914dd3104244d3089
dinecaleb <calebtonyenwin@rocketmail.com>
2021-06-05 19:49:06 -0400

    use custom component class

--- modal.js
+++ modal.js
@@ -1,3 +1,38 @@
+    
+    
+    class Modal extends HTMLElement {
+        constructor() {
+          super();
+        }
+
+        
+      connectedCallback() {
+        this.innerHTML = `
+        <link rel="stylesheet" type="text/css" media="screen" href="modal.css" />
+          <div class="modal">
+          <div class="modal-content">
+            <span class="close-button" onclick="toggleModal()">&times;</span>
+    
+            <div class="modalBody">
+              <h3 id="modalQuestion"></h3>
+    
+              <div class="confirmButtons">
+                <button class="confirmBtn" onclick="confirm('Yes')">Yes</button>
+                <button class="confirmBtn" onclick="confirm('Cancel')">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+      }
+    }
+
+    
+    customElements.define('modal-component', Modal);
+
+
     const modal = document.querySelector(".modal");   //modal reference
 
     function toggleModal() {  //reusable function to toggle the modal
