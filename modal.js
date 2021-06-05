51989f963c649f4dab421e47aa5efc2c04f2df3a
dinecaleb <calebtonyenwin@rocketmail.com>
2021-06-05 11:06:26 -0400

    added more comments

--- modal.js
+++ modal.js
@@ -1,22 +1,22 @@
-    const modal = document.querySelector(".modal");
+    const modal = document.querySelector(".modal");   //modal reference
 
     function toggleModal() {  //reusable function to toggle the modal
         modal.classList.toggle("show-modal");
     }
 
-    function showModal(question) {
+    function showModal(question) { //display modal
         document.getElementById("clickConfirmText").innerHTML = null;  //clear any existing confirmation text;
         document.getElementById("modalQuestion").innerHTML = question;
         toggleModal()
     }
 
-    function confirm(option){
+    function confirm(option){  //display confirmation text 
         toggleModal() 
         document.getElementById("clickConfirmText").innerHTML = `You just clicked "${option}"`;
     }
 
-    function modalBodyClicked(event) {
-        if (event.target === modal) {  //if the modal body is clicked, hide the modal
+    function modalBodyClicked(event) { //if the modal body is clicked, hide the modal
+        if (event.target === modal) {  
             toggleModal();
         }
     }
